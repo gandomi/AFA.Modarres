fb6187eb108ef7d31eb23856a0ade0933c48093c
Ali Gandomi <gandomi313@gmail.com>
2017-08-31 23:58:50 +0430

    Add my constants

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,16 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+/*
+|--------------------------------------------------------------------------
+| My Constants
+|--------------------------------------------------------------------------
+|
+*/
+define('_HOST_', 'localhost');
+define('_SAFE_INPUT_', 'trim|stripslashes|htmlspecialchars');
+define('_SALT_', '');
+define('ENCRYPT_KEY', '');
+define('CookieName', 'GandomCookie');
